5ec53e0d3259db5832dd0ce6f1858405de03d6ba
hrameshn <hrameshn@INLEN8520030622.corp.capgemini.com>
2023-04-01 15:01:40 +0530

    1commit

--- GTPLBank/src/loginPage/VerifyLoginPage.java
+++ GTPLBank/src/loginPage/VerifyLoginPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class VerifyLoginPage {
 	
 	
-
+   //verify login page
 	public static void main(String[]args) {
 		
 		WebDriver driver= new ChromeDriver();
